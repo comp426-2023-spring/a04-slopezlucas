6bbb455ae0d8cd99a4a36b45af3f3e74898760c0
root <root@DESKTOP-D4RTLKG.localdomain>
2023-05-08 20:38:03 -0400

    first

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import express from 'express';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const argv = minimist(process.argv.slice(2));
 const port = argv.port || 5000;
